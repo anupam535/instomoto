2a1c3f5135f4e25c34e53bac3dce9af958657796
Pritam Kumar <97377005+anupam535@users.noreply.github.com>
2025-02-20 23:15:36 +0530

    Create config.py

---
+++ core/config.py
@@ -0,0 +1,9 @@
+TELEGRAM_BOT_TOKEN = "7604424348:AAHtkmD0YKApTg4B9-QeJNW5SXUmOmctK7E"
+
+# Instagram Login Credentials (for testing purposes)
+INSTAGRAM_USERNAME = "akira_y.official"
+INSTAGRAM_PASSWORD = "323416"
+
+# Proxy Settings
+USE_PROXY = True  # Set False if you don't want to use a proxy
+PROXY_LIST = "data/proxies.json"
